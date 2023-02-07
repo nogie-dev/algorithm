eff964143bb07763510c38df9d1d1acc634bd9a7
nogie <86417883+nogie-dev@users.noreply.github.com>
2023-02-07 23:56:52 +0900

    Create triangle.py

---
+++ class2/triangle.py
@@ -0,0 +1,17 @@
+import sys
+
+lst=[]
+
+while True:
+    tmp=input().split(" ")
+    lst.append(tmp)
+    
+    if lst[len(lst)-1][0] == "0" and lst[len(lst)-1][1] == "0" and lst[len(lst)-1][2] == "0":
+        break
+    else:
+
+for i in range(len(lst)):
+    max(lst[i][0],lst[i][1],lst[i][2])
+    
+
+    
