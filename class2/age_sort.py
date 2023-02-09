26c55cb77bd9666049dc6ccfdac7790afeca1157
nogie <86417883+nogie-dev@users.noreply.github.com>
2023-02-09 23:00:16 +0900

    Update age_sort.py

--- class2/age_sort.py
+++ class2/age_sort.py
@@ -9,3 +9,12 @@ tmp=sorted(lst,key=lambda x:int(x[0]))
 
 for a,n in tmp:
     print(a,n)
+
+#another ver
+n=input()
+lst=[]
+for _ in range(int(n)):
+    lst.append(tuple(input().split(" ")))
+lst.sort(key=lambda x:int(x[0]))
+for a,n in lst:
+    print(a,n)
