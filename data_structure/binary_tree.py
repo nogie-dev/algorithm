6c0b8a174d65ca4b4d6398923e3de2833d8e9942
nogie <86417883+nogie-dev@users.noreply.github.com>
2023-02-14 23:42:06 +0900

    Update binary_tree.py

--- data_structure/binary_tree.py
+++ data_structure/binary_tree.py
@@ -12,39 +12,43 @@ class root:
         self.root=None
         
     def preorder(self,t):
-        print(t.data)
-        if t.left!=None:
-            print(t.left.data)
-        if t.right!=None:
-            print(t.right.data)
+        if t!=None:
+            print(t.data)
+            if t.left!=None:
+                self.preorder(t.left)
+            if t.right!=None:
+                self.preorder(t.right)
             
     def inorder(self,t):
-        if t.left!=None:
-            print(t.left.data)
-        print(t.data)
-        if t.right!=None:
-            print(t.right.data)
+        if t!=None:
+            if t.left!=None:
+                self.preorder(t.left)
+            print(t.data)
+            if t.right!=None:
+                self.preorder(t.right)
             
     def postorder(self,t):
-        if t.left!=None:
-            print(t.left.data)
-        if t.right!=None:
-            print(t.right.data)
-        print(t.data)
+        if t!=None:
+            print(t.data)
+            if t.left!=None:
+                self.preorder(t.left)
+            if t.right!=None:
+                self.preorder(t.right)
+            print(t.data)
     
 t=root()
 
 n1=Node(10)
 n2=Node(20)
 n3=Node(30)
+n4=Node(40)
+n5=Node(50)
 
 t.root=n1
 n1.left=n2
 n1.right=n3
+n2.left=n4
+n2.right=n5
+
 
 t.preorder(n1)
-t.inorder(n1)
-
-
-
-    
