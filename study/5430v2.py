8a6d11e2a7106a85f0636d071029386fc6b4e019
nogie <86417883+nogie-dev@users.noreply.github.com>
2023-03-18 20:22:07 +0900

    Create 5430v2.py

---
+++ study/5430v2.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+ir=[]
+buf=deque()
+check_buf_len=deque()
+result=[]
+
+test_case=int(input())
+for i in range(test_case):
+    ir.append(input())
+    check_buf_len.append(int(input()))
+    
+    tmp=input().replace('[',"").replace("]","")
+    if len(tmp)==0:
+        buf.append(deque(""))
+    else:
+        buf.append(deque(list(map(int,tmp.split(",")))))
+    
+for lst in range(len(buf)):
+    ir_tmp=ir[lst]
+    buf_tmp=buf.popleft()
+    err_flag=False
+    
+    for i in range(len(ir_tmp)):
+        #print("ir:",ir_tmp[i])
+        if ir_tmp[i]=="R":
+            buf_tmp=deque(list(buf_tmp)[::-1])
+            #print("this:",buf_tmp)
+            
+        if ir_tmp[i]=="D" and len(buf_tmp)!=0:
+            buf_tmp.popleft()
+        elif ir_tmp[i]=="D" and len(buf_tmp)==0:
+            err_flag=True
+            break
+    
+    if err_flag:
+        result.append("error")
+    else:
+        result.append(list(buf_tmp))
+        
+for i in result:
+    print(i)
+            
+        
